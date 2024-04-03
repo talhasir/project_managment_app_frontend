d91a7ea131b1db23f8acec1a682c24d2e057c675
talhaSir1 <talhahafiz145@gmail.com>
2024-04-03 17:32:04 +0500

    Added Filter and sorting by ant table in project table

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PageComponent from "../Layouts/ViewLayout";
-import {
-  Button,
-  Input,
-  Popover,
-  Space,
-  Table,
-  Select,
-} from "antd";
+import { Button, Input, Popover, Space, Table, Select } from "antd";
 import axiosClient from "../axiosClient";
 import { useSearchParams } from "react-router-dom";
+import { SearchOutlined } from "@ant-design/icons";
+import Highlighter from "react-highlight-words";
 
 function Projects(props) {
   const [projects, setProjects] = useState([]);
   const [pagination, setPagination] = useState({});
   const [loading, setLoading] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams({});
+  const [sortParams, setSortParams] = useState({});
+  const [queryParams, setQueryParams] = useState();
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
 
-  const fetchProjects = async (page = 1, filters) => {
+  const fetchProjects = async (page = 1, filters, sorter) => {
     setLoading(true);
     try {
-      const res = await axiosClient.get(`/projects?page=${page}`, {
-        params: filters,
+      const res = await axiosClient.get("/projects", {
+        params: { page, filters, sorter },
       });
-      const { data, pagination } = res?.data?.projects;
+      const { data, pagination, queryParams } = res?.data?.projects;
       setProjects(data);
       setLoading(false);
-      console.log(res);
+      setQueryParams(queryParams && queryParams);
       setPagination({
         current: pagination?.current_page,
         pageSize: pagination?.per_page,
@@ -41,6 +41,17 @@ function Projects(props) {
   useEffect(() => {
     fetchProjects();
   }, []);
+
+  useEffect(() => {
+    // debugger;
+    if (searchParams) {
+      handleTableChange("", searchParams, sortParams);
+      // setSortParams({
+      //   sortField: "name",
+      //   sortDirection: "asc",
+      // });
+    }
+  }, [searchParams]);
 
   const PORJECT_STATUS_TEXT = {
     completed: "Completed",
@@ -63,6 +74,118 @@ function Projects(props) {
     in_process: "In Process",
   };
 
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
   const columns = [
     {
       title: "ID",
@@ -74,6 +197,12 @@ function Projects(props) {
       title: "NAME",
       dataIndex: "name",
       key: "name",
+      showSorterTooltip: {
+        target: "full-header",
+      },
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ["descend"],
+      ...getColumnSearchProps("name"),
     },
     {
       title: "IMAGE",
@@ -149,13 +278,14 @@ function Projects(props) {
   ];
 
   const handleTableChange = (pagination, filters, sorter) => {
-    fetchProjects(pagination.current, filters);
+    fetchProjects(pagination.current, filters, sorter);
+    console.log(filters);
   };
 
   const SELECT_STATUS_OPTIONS = [
-    { lable: "completed", value: "Completed" },
-    { lable: "pending", value: "Pending" },
-    { lable: "in_process", value: "In Process" },
+    { lable: "Completed", value: "completed" },
+    { lable: "Pending", value: "pending" },
+    { lable: "In Process", value: "in_process" },
   ];
 
   return (
@@ -167,35 +297,46 @@ function Projects(props) {
             name="search"
             size="large"
             className="col-span-3"
+            value={queryParams?.search}
             onChange={(e) =>
               setSearchParams({ ...searchParams, search: e.target.value })
             }
-            onKeyDown={(e) =>
-              e.key === "Enter" ? fetchProjects("", searchParams) : ""
-            }
+            // onKeyDown={(e) =>
+            //   e.key === "Enter" ? fetchProjects("", searchParams) : ""
+            // }
           />
           <Select
             defaultValue={"Filter By Status"}
             size="large"
             className="col-span-2"
             options={SELECT_STATUS_OPTIONS}
+            value={queryParams?.select}
             onChange={(value) =>
               setSearchParams({ ...searchParams, select: value })
             }
           />
-          <Button type="primary" size="large">
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => {
+              setQueryParams({});
+              setSearchParams({});
+            }}
+          >
             Reset
           </Button>
         </div>
-        <Table
-          loading={loading}
-          bordered
-          sticky
-          columns={columns}
-          dataSource={projects}
-          pagination={pagination}
-          onChange={handleTableChange}
-        />
+        <div className="overflow-auto">
+          <Table
+            loading={loading}
+            sticky
+            columns={columns}
+            dataSource={projects}
+            pagination={pagination}
+            onChange={handleTableChange}
+            bordered
+          />
+        </div>
       </>
     </PageComponent>
   );
